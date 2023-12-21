35a2e240e62480e2b0a0d98eadd60a623bf66ae7
FIameCaster <82079841+FIameCaster@users.noreply.github.com>
2023-12-21 16:20:27 +0100

    Small refactor

--- package/src/extensions/matchBrackets/index.ts
+++ package/src/extensions/matchBrackets/index.ts
@@ -7,7 +7,7 @@ const closingCharCodes: boolean[] = []
 
 export interface BracketMatcher extends Extension {
 	/**
-	 * Array of tuples containing:
+	 * Array of tuples containing in the following order:
 	 * - The bracket's `Token`
 	 * - Its starting position
 	 * - Its level of nesting
@@ -24,63 +24,63 @@ export type Bracket = [Prism.Token, number, number, string, boolean]
 /**
  * Extension that matches brackets together.
  * @param rainbowBrackets Whether to add extra classes to brackets for styling. Defaults to true.
- * Adding the extension dynamically, will force a rerender to apply those extra classes.
+ * Adding the extension dynamically, will force a rerender to add those extra classes.
  *
  * Without rainbow brackets, this extension can be added dynamically with no side effects.
  */
 export const matchBrackets = (rainbowBrackets = true): BracketMatcher => {
-	let brackets: Bracket[] = [],
-		stack: [number, number][],
-		bracketIndex: number,
-		pairMap: number[] = [],
-		matchBrackets = (obj: { tokens: (Prism.Token | string)[] }) => {
-			stack = []
-			pairMap.length = brackets.length = bracketIndex = 0
-			matchRecursive(obj.tokens, 0)
-			if (rainbowBrackets) {
-				for (let i = 0, bracket: Bracket; (bracket = brackets[i]); ) {
-					let alias = bracket[0].alias
+	let stack: [number, number][]
+	let bracketIndex: number
+	const brackets: Bracket[] = []
+	const pairMap: number[] = []
+	const matchBrackets = (obj: { tokens: (Prism.Token | string)[] }) => {
+		stack = []
+		pairMap.length = brackets.length = bracketIndex = 0
+		matchRecursive(obj.tokens, 0)
+		if (rainbowBrackets) {
+			for (let i = 0, bracket: Bracket; (bracket = brackets[i]); ) {
+				let alias = bracket[0].alias
 
-					bracket[0].alias =
-						(alias ? ((<[]>alias).join?.(" ") || alias) + " " : "") +
-						`bracket-${pairMap[i++] == null ? "error" : "level-" + (bracket[2] % 12)}`
-				}
+				bracket[0].alias =
+					(alias ? ((<[]>alias).join?.(" ") || alias) + " " : "") +
+					`bracket-${pairMap[i++] == null ? "error" : "level-" + (bracket[2] % 12)}`
 			}
-		},
-		matchRecursive = (tokens: (Prism.Token | string)[], position: number) => {
-			for (let i = 0, token: string | Prism.Token; (token = tokens[i++]); ) {
-				if (typeof token != "string") {
-					const type = token.type,
-						content = <string | (string | Prism.Token)[]>token.content
+		}
+	}
+	const matchRecursive = (tokens: (Prism.Token | string)[], position: number) => {
+		for (let i = 0, token: string | Prism.Token; (token = tokens[i++]); ) {
+			if (typeof token != "string") {
+				const type = token.type,
+					content = <string | (string | Prism.Token)[]>token.content
 
-					if (type != "regex") {
-						if (Array.isArray(content)) {
-							matchRecursive(content, position)
-						} else if ((token.alias || type) == "punctuation") {
-							let charCode = content.charCodeAt(content.length - 1),
-								isOpen = !!openingCharCodes[charCode]
-							if (isOpen || closingCharCodes[charCode]) {
-								brackets[bracketIndex] = [token, position, 0, content, isOpen]
+				if (type != "regex") {
+					if (Array.isArray(content)) {
+						matchRecursive(content, position)
+					} else if ((token.alias || type) == "punctuation") {
+						let charCode = content.charCodeAt(content.length - 1),
+							isOpen = !!openingCharCodes[charCode]
+						if (isOpen || closingCharCodes[charCode]) {
+							brackets[bracketIndex] = [token, position, 0, content, isOpen]
 
-								if (isOpen) stack.push([bracketIndex, charCode])
-								else {
-									for (let i = stack.length; i; ) {
-										let [index, charCode1] = stack[--i]
-										if (charCode - charCode1 < 3 && charCode - charCode1 > 0) {
-											pairMap[(pairMap[bracketIndex] = index)] = bracketIndex
-											brackets[bracketIndex][2] = brackets[index][2] = stack.length = i
-											break
-										}
+							if (isOpen) stack.push([bracketIndex, charCode])
+							else {
+								for (let i = stack.length; i; ) {
+									let [index, charCode1] = stack[--i]
+									if (charCode - charCode1 < 3 && charCode - charCode1 > 0) {
+										pairMap[(pairMap[bracketIndex] = index)] = bracketIndex
+										brackets[bracketIndex][2] = brackets[index][2] = stack.length = i
+										break
 									}
 								}
-								bracketIndex++
 							}
+							bracketIndex++
 						}
 					}
 				}
-				position += token.length
 			}
+			position += token.length
 		}
+	}
 
 	return {
 		update(editor) {
