def479329e22169bde30ecec8563cc38f41f8714
FIameCaster <82079841+FIameCaster@users.noreply.github.com>
2024-02-05 21:59:26 +0100

    Fix calling clone with multiple arguments

--- package/src/prism/utils/language.js
+++ package/src/prism/utils/language.js
@@ -1,26 +1,28 @@
 import { languages, rest, tokenize } from "../core.js";
 
-var clone = (o, visited = new Map) => {
+var _clone = (o, visited) => {
 	if (visited.has(o)) return visited.get(o);
 	var copy = o, t = toString.call(o).slice(8, -1);
 
 	if (t == "Object") {
 		visited.set(o, copy = {});
 		for (var key in o) {
-			copy[key] = clone(o[key], visited);
+			copy[key] = _clone(o[key], visited);
 		}
-		if (o[rest]) copy[rest] = clone(o[rest], visited);
+		if (o[rest]) copy[rest] = _clone(o[rest], visited);
 		if (o[tokenize]) copy[tokenize] = o[tokenize];
 	}
 	else if (t == "Array") {
 		visited.set(o, copy = []);
 		for (var i = 0, l = o.length; i < l; i++) {
-			copy[i] = clone(o[i], visited);
+			copy[i] = _clone(o[i], visited);
 		}
 	}
 
 	return copy;
 }
+
+var clone = o => _clone(o, new Map);
 
 var extend = (id, redef) => Object.assign(clone(languages[id]), redef);
 
