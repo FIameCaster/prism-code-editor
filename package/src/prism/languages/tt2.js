42c59ec64b22f63d1f36d4b280174ac99cc0aad6
FIameCaster <82079841+FIameCaster@users.noreply.github.com>
2024-03-17 23:53:18 +0100

    Merge tt2 strings

--- package/src/prism/languages/tt2.js
+++ package/src/prism/languages/tt2.js
@@ -6,12 +6,25 @@ import './markup.js';
 
 var tt2 = extend('clike', {
 	'comment': /#.*|\[%#[\s\S]*?%\]/,
+	'string': [
+		{
+			pattern: /'[^\\']*(?:\\[\s\S][^\\']*)*'/g,
+			greedy: true,
+		},
+		{
+			pattern: /"[^\\"]*(?:\\[\s\S][^\\"]*)*"/g,
+			greedy: true,
+			inside: {
+				'variable': /\$(?:[a-z]\w*(?:\.(?:\d+|\$?[a-z]\w*))*)/i
+			}
+		}
+	],
 	'keyword': /\b(?:BLOCK|CALL|CASE|CATCH|CLEAR|DEBUG|DEFAULT|ELSE|ELSIF|END|FILTER|FINAL|FOREACH|[GS]ET|IF|IN|INCLUDE|INSERT|LAST|MACRO|META|NEXT|PERL|PROCESS|RAWPERL|RETURN|STOP|SWITCH|TAGS|THROW|TRY|UNLESS|USE|WHILE|WRAPPER)\b/,
 	'punctuation': /[()[\]{},]/
 });
 
 insertBefore(tt2, 'number', {
-	'operator': /=[>=]?|!=?|<=?|>=?|&&|\|\|?|\b(?:and|not|or)\b/,
+	'operator': /=>|[!=<>]=?|&&|\|\|?|\b(?:and|not|or)\b/,
 	'variable': /\b[a-z]\w*(?:\s*\.\s*(?:\d+|\$?[a-z]\w*))*\b/i
 });
 
@@ -21,25 +34,6 @@ insertBefore(tt2, 'keyword', {
 		alias: 'punctuation'
 	}
 });
-
-insertBefore(tt2, 'string', {
-	'single-quoted-string': {
-		pattern: /'[^\\']*(?:\\[\s\S][^\\']*)*'/g,
-		greedy: true,
-		alias: 'string'
-	},
-	'double-quoted-string': {
-		pattern: /"[^\\"]*(?:\\[\s\S][^\\"]*)*"/g,
-		greedy: true,
-		alias: 'string',
-		inside: {
-			'variable': /\$(?:[a-z]\w*(?:\.(?:\d+|\$?[a-z]\w*))*)/i
-		}
-	}
-});
-
-// The different types of TT2 strings "replace" the C-like standard string
-delete tt2.string;
 
 languages.tt2 = {
 	'tt2': {
