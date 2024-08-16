3fe66f7b588fe130ddee43abb2f3daa2bf58443a
FIameCaster <82079841+FIameCaster@users.noreply.github.com>
2024-08-16 16:14:50 +0200

    Add registerTheme function

--- package/src/themes/index.ts
+++ package/src/themes/index.ts
@@ -1,5 +1,20 @@
 const themes = import.meta.glob("./*.css", { query: "?inline" })
 
-/** Asynchronously loads the theme with the specified name. */
-export const loadTheme = async (name: string) =>
+/**
+ * Function that allows overriding the default themes or registering new themes to be
+ * used by the setups or web components.
+ * @param name Name of the theme.
+ * @param loader Function returning a promise that resolves with the CSS for the theme.
+ */
+const registerTheme = (name: string, loader: () => Promise<{ default: string }>) => {
+	themes[`./${name}.css`] = loader
+}
+
+/**
+ * Asynchronously loads the theme with the specified name.
+ * The promise returned resolves with the CSS string for the theme.
+ */
+const loadTheme = async (name: string) =>
 	(<undefined | { default: string }>await themes[`./${name}.css`]?.())?.default
+
+export { loadTheme, registerTheme }
