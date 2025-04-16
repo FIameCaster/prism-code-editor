7f2b1919c3b2ee02035d606f2e18cc22165aedd8
FIameCaster <82079841+FIameCaster@users.noreply.github.com>
2025-04-16 17:50:41 +0200

    Add new entry points to package

--- react-package/vite.config.ts
+++ react-package/vite.config.ts
@@ -13,16 +13,27 @@ const entries: Record<string, string> = {
 	"extensions/match-tags": "src/extensions/match-tags.ts",
 	"extensions/search/index": "src/extensions/search/index.ts",
 	search: "src/extensions/search/search.css",
+	invisibles: "src/extensions/search/invisibles.css",
 	copy: "src/extensions/copy-button/copy.css",
 	"extensions/copy-button/index": "src/extensions/copy-button/index.ts",
 	"extensions/folding/index": "src/extensions/folding/index.ts",
+	"extensions/autocomplete/index": "src/extensions/autocomplete/index.ts",
+	"extensions/autocomplete/css/index": "src/extensions/autocomplete/css/index.ts",
+	"extensions/autocomplete/markup/index": "src/extensions/autocomplete/markup/index.ts",
+	"extensions/autocomplete/javascript/index": "src/extensions/autocomplete/javascript/index.ts",
+	"extensions/autocomplete/svelte/index": "src/extensions/autocomplete/svelte/index.ts",
+	"extensions/autocomplete/vue/index": "src/extensions/autocomplete/vue/index.ts",
+	autocomplete: "src/extensions/autocomplete/style.css",
+	"autocomplete-icons": "src/extensions/autocomplete/icons.css",
 	"extensions/overscroll": "src/extensions/overscroll.ts",
+	"code-block/index": "src/code-block/index.tsx",
 	folding: "src/extensions/folding/folding.css",
 	setups: "src/setups.tsx",
 	tooltips: "src/tooltips.ts",
 	scrollbar: "src/scrollbar.css",
 	"rtl-layout": "src/rtl-layout.css",
 	layout: "src/layout.css",
+	"code-block": "src/code-block.css",
 	"themes/index": "src/themes/index.ts",
 	"utils/index": "src/utils/index.ts",
 	"prism/index": "src/prism/index.js",
@@ -53,11 +64,6 @@ await Promise.all([
 
 export default defineConfig({
 	plugins: [
-		/* 
-    Uncomment the following line to enable solid-devtools.
-    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
-    */
-		// devtools(),
 		react(),
 		dts({
 			beforeWriteFile(filePath) {
